60c6542ca385de81a8864ed2d701236d637262ae
LakeLove <lakegreene@yahoo.com>
2020-04-27 20:43:17 -0400

    Added introduction method.

--- src/TAG.java
+++ src/TAG.java
@@ -5,4 +5,7 @@ public class TAG {
     Scanner scanner = new Scanner(System.in);
     public static void main(String[] args) {
     }
+    public static String introduction() {
+        return ("Working Title\nby Lake Greene\n\nWelcome home.");
+    }
 }
