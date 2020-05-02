91c16c54a42662761c29cc70c26882358206fdc2
LakeLove <lakegreene@yahoo.com>
2020-05-01 22:56:26 -0400

    Created tests describe_LocationYard() and getChoices_LocationYard().

--- test/WorldTest.java
+++ test/WorldTest.java
@@ -1,4 +1,7 @@
 import org.junit.Test;
+
+import java.util.ArrayList;
+
 import static org.junit.Assert.*;
 
 public class WorldTest {
@@ -7,6 +10,21 @@ public class WorldTest {
     public void describe_LocationYard() {
         String expectedResult = "You are at the yard.";
         String actualResult = test_World.describe();
+        assertEquals(expectedResult, actualResult);
+    }
+
+    @Test
+    public void getChoices_LocationYard() {
+        ArrayList<String> expectedResult = new ArrayList<>();
+        expectedResult.add("Quit"); expectedResult.add("Look at yard");
+        ArrayList<String> actualResult = test_World.getChoices(test_World);
+        assertEquals(expectedResult, actualResult);
+    }
+
+    @Test
+    public void getLookChoices_LocationYard() {
+        String expectedResult = "Look at yard";
+        String actualResult = test_World.getLookChoices(test_World);
         assertEquals(expectedResult, actualResult);
     }
 }
