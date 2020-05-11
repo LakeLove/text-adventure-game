ff0394434d2d6d8d938f4559eba41bb63ea2771b
LakeLove <lakegreene@yahoo.com>
2020-05-11 19:43:51 -0400

    Created tests update_LocationEntrance() and go_LocationWoods() in WorldTest class.

--- src/World.java
+++ src/World.java
@@ -3,89 +3,77 @@ import java.util.HashMap;
 import java.util.Objects;
 
 public class World {
-    String status;
-    Player you;
-    HashMap<String, Location> locations;
-
-    public World() {
-        status = "playing";
-        you = new Player();
-        locations = new HashMap<>();
-        locations.put("yard", new Location("There are many weeds, surrounding a big spooky house."));
-        locations.put("woods", new Location("It is dark here. You are likely to be eaten by a grue."));
-        locations.put("entrance", new Location("You see stairs, and what appears to be a dining room."));
-        locations.put("dining room", new Location("There are appears to be old, rotting food on the table."));
-        locations.put("upstairs", new Location("There is a locked door."));
-    }
-
-    static String update(World world, String command) {
-        String commandVerb = TAG.getCommandVerb(command);
-        switch (commandVerb) {
-            case "quit":
-                return quit(world);
-            case "look":
-                return look(world, command);
-            case "go":
-                return go(world, command);
-            default:
-                return "Error";
-        }
-    }
-
-    static String quit(World world) {
-        world.status = "quitting";
-        return "You chose to quit the game.";
-    }
-
-    static String look(World world, String command) {
-        String lookingAt = TAG.chomp(command, "look at ");
-        Location location = world.locations.get(lookingAt);
-        return location.about;
-    }
-
-    static String go(World world, String command) {
-        String newLocation = TAG.chomp(command, "go to ");
-        world.you.at = newLocation;
-        return "You went to " + newLocation;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (!(o instanceof World)) return false;
-        World world = (World) o;
-        return status.equals(world.status) &&
-                you.equals(world.you) &&
-                locations.equals(world.locations);
-    }
-    @Override
-    public int hashCode() {
-        return Objects.hash(status, you, locations);
-    }
-
-    String describe() {
-        return String.format("You are at the %s.", you.at);
-    }
-
-    static ArrayList<String> getChoices(World world) {
-        ArrayList<String> choices = new ArrayList<>();
-        choices.add("Quit");
-        choices.add(getLookChoices(world));
-        choices.addAll(getGoChoices(world));
-        return choices;
-    }
-
-    static String getLookChoices(World world) {
-        String yourLocation = world.you.at;
-        return "Look at " + yourLocation;
-    }
-
-    static ArrayList<String> getGoChoices(World world) {
-        ArrayList<String> choices = new ArrayList<>();
-        HashMap<String, Location> possibleLocations = world.locations;
-        for (Object locationName : possibleLocations.keySet()) {
-            choices.add("Go to " + locationName);
-        }
-        return choices;
-    }
+	String status;
+	Player you;
+	HashMap<String, Location> locations;
+	public World() {
+		status = "playing";
+		you = new Player();
+		locations = new HashMap<>();
+		locations.put("yard", new Location("There are many weeds, surrounding a big spooky house."));
+		locations.put("woods", new Location("It is dark here. You are likely to be eaten by a grue."));
+		locations.put("entrance", new Location("You see stairs, and what appears to be a dining room."));
+		locations.put("dining room", new Location("There are appears to be old, rotting food on the table."));
+		locations.put("upstairs", new Location("There is a locked door."));
+	}
+	static String update(World world, String command) {
+		String commandVerb = TAG.getCommandVerb(command);
+		switch (commandVerb) {
+			case "quit":
+				return quit(world);
+			case "look":
+				return look(world, command);
+			case "go":
+				return go(world, command);
+			default:
+				return "Error";
+		}
+	}
+	static String quit(World world) {
+		world.status = "quitting";
+		return "You chose to quit the game.";
+	}
+	static String look(World world, String command) {
+		String lookingAt = TAG.chomp(command, "look at ");
+		Location location = world.locations.get(lookingAt);
+		return location.about;
+	}
+	static String go(World world, String command) {
+		String newLocation = TAG.chomp(command, "go to ");
+		world.you.at = newLocation;
+		return "You went to " + newLocation;
+	}
+	static ArrayList<String> getChoices(World world) {
+		ArrayList<String> choices = new ArrayList<>();
+		choices.add("Quit");
+		choices.add(getLookChoices(world));
+		choices.addAll(getGoChoices(world));
+		return choices;
+	}
+	static String getLookChoices(World world) {
+		String yourLocation = world.you.at;
+		return "Look at " + yourLocation;
+	}
+	static ArrayList<String> getGoChoices(World world) {
+		ArrayList<String> choices = new ArrayList<>();
+		HashMap<String, Location> possibleLocations = world.locations;
+		for (Object locationName : possibleLocations.keySet()) {
+			choices.add("Go to " + locationName);
+		}
+		return choices;
+	}
+	@Override
+	public boolean equals(Object o) {
+		if (this == o) return true;
+		if (!(o instanceof World)) return false;
+		World world = (World) o;
+		return status.equals(world.status) && you.equals(world.you) && locations.equals(world.locations);
+	}
+	@Override
+	public int hashCode() {
+		return Objects.hash(status, you, locations);
+	}
+	String describe() {
+		return String.format("You are at the %s.", you.at);
+	}
 }
--- test/WorldTest.java
+++ test/WorldTest.java
@@ -4,78 +4,97 @@ import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 
 public class WorldTest {
-    @Test
-    public void world_AllLocations() {
-        World test_World = new World();
-        Assert.assertTrue(test_World.locations.containsKey("yard"));
-        Assert.assertTrue(test_World.locations.containsKey("woods"));
-        Assert.assertTrue(test_World.locations.containsKey("entrance"));
-        Assert.assertTrue(test_World.locations.containsKey("dining room"));
-        Assert.assertTrue(test_World.locations.containsKey("upstairs"));
-        assertEquals(5, test_World.locations.size());
-    }
-    @Test
-    public void describe_LocationYard() {
-        World test_World = new World();
-        String expectedResult = "You are at the yard.";
-        String actualResult = test_World.describe();
-        assertEquals(expectedResult, actualResult);
-    }
-    @Test
-    public void getChoices_AllChoices() {
-        World test_World = new World();
-        Assert.assertTrue(World.getChoices(test_World).contains("Quit"));
-        Assert.assertTrue(World.getChoices(test_World).contains("Look at yard"));
-        Assert.assertTrue(World.getChoices(test_World).contains("Go to yard"));
-        Assert.assertTrue(World.getChoices(test_World).contains("Go to woods"));
-        Assert.assertTrue(World.getChoices(test_World).contains("Go to entrance"));
-        Assert.assertTrue(World.getChoices(test_World).contains("Go to dining room"));
-        Assert.assertTrue(World.getChoices(test_World).contains("Go to upstairs"));
-        assertEquals(7, World.getChoices(test_World).size());
-    }
-    @Test
-    public void getLookChoices_LocationYard() {
-        World test_World = new World();
-        String expectedResult = "Look at yard";
-        String actualResult = World.getLookChoices(test_World);
-        assertEquals(expectedResult, actualResult);
-    }
-    @Test
-    public void getGoChoices_AllLocations() {
-        World test_World = new World();
-        Assert.assertTrue(World.getGoChoices(test_World).contains("Go to yard"));
-        Assert.assertTrue(World.getGoChoices(test_World).contains("Go to woods"));
-        Assert.assertTrue(World.getGoChoices(test_World).contains("Go to entrance"));
-        Assert.assertTrue(World.getGoChoices(test_World).contains("Go to dining room"));
-        Assert.assertTrue(World.getGoChoices(test_World).contains("Go to upstairs"));
-        assertEquals(5, World.getGoChoices(test_World).size());
-    }
-    @Test
-    public void update_LocationYard_StatusQuitting() {
-        World test_World = new World();
-        String expectedResult = "There are many weeds, surrounding a big spooky house.";
-        String actualResult = World.look(test_World, "look at yard");
-        assertEquals(expectedResult, actualResult);
-        actualResult = World.update(test_World, "quit");
-        String expectedResult2 = "You chose to quit the game.";
-        assertEquals(expectedResult2, actualResult);
-        assertEquals(test_World.status, "quitting");
-    }
-    @Test
-    public void look_LocationYard() {
-        World test_World = new World();
-        String expectedResult = "There are many weeds, surrounding a big spooky house.";
-        String actualResult = World.look(test_World, "look at yard");
-        assertEquals(expectedResult, actualResult);
-        World test_World2 = new World();
-        assertEquals(test_World2, test_World);
-    }
-    @Test
-    public void quit_ExpectedResult() {
-        World test_World = new World();
-        String expectedResult = "You chose to quit the game.";
-        String actualResult = World.quit(test_World);
-        assertEquals(expectedResult, actualResult);
-        assertEquals(test_World.status, "quitting");
-    }
+	@Test
+	public void world_AllLocations() {
+		World test_World = new World();
+		Assert.assertTrue(test_World.locations.containsKey("yard"));
+		Assert.assertTrue(test_World.locations.containsKey("woods"));
+		Assert.assertTrue(test_World.locations.containsKey("entrance"));
+		Assert.assertTrue(test_World.locations.containsKey("dining room"));
+		Assert.assertTrue(test_World.locations.containsKey("upstairs"));
+		assertEquals(5, test_World.locations.size());
+	}
+	@Test
+	public void update_LocationYard_StatusQuitting() {
+		World test_World = new World();
+		String expectedResult = "There are many weeds, surrounding a big spooky house.";
+		String actualResult = World.look(test_World, "look at yard");
+		assertEquals(expectedResult, actualResult);
+		actualResult = World.update(test_World, "quit");
+		String expectedResult2 = "You chose to quit the game.";
+		assertEquals(expectedResult2, actualResult);
+		assertEquals(test_World.status, "quitting");
+	}
+	@Test
+	public void update_LocationEntrance() {
+		World test_World = new World();
+		String expectedResult = "You went to entrance";
+		String actualResult = World.update(test_World, "go to entrance");
+		assertEquals(expectedResult, actualResult);
+		assertEquals("entrance", test_World.you.at);
+		String expectedResult2 = "You see stairs, and what appears to be a dining room.";
+		actualResult = World.update(test_World, "look at entrance");
+		assertEquals(expectedResult2, actualResult);
+	}
+	@Test
+	public void quit_ExpectedResult() {
+		World test_World = new World();
+		String expectedResult = "You chose to quit the game.";
+		String actualResult = World.quit(test_World);
+		assertEquals(expectedResult, actualResult);
+		assertEquals(test_World.status, "quitting");
+	}
+	@Test
+	public void look_LocationYard() {
+		World test_World = new World();
+		String expectedResult = "There are many weeds, surrounding a big spooky house.";
+		String actualResult = World.look(test_World, "look at yard");
+		assertEquals(expectedResult, actualResult);
+		World test_World2 = new World();
+		assertEquals(test_World2, test_World);
+	}
+	@Test
+	public void go_LocationWoods() {
+		World test_World = new World();
+		String expectedResult = "You went to woods";
+		String actualResult = World.go(test_World, "go to woods");
+		assertEquals(expectedResult, actualResult);
+		assertEquals("woods", test_World.you.at);
+	}
+	@Test
+	public void describe_LocationYard() {
+		World test_World = new World();
+		String expectedResult = "You are at the yard.";
+		String actualResult = test_World.describe();
+		assertEquals(expectedResult, actualResult);
+	}
+	@Test
+	public void getChoices_AllChoices() {
+		World test_World = new World();
+		Assert.assertTrue(World.getChoices(test_World).contains("Quit"));
+		Assert.assertTrue(World.getChoices(test_World).contains("Look at yard"));
+		Assert.assertTrue(World.getChoices(test_World).contains("Go to yard"));
+		Assert.assertTrue(World.getChoices(test_World).contains("Go to woods"));
+		Assert.assertTrue(World.getChoices(test_World).contains("Go to entrance"));
+		Assert.assertTrue(World.getChoices(test_World).contains("Go to dining room"));
+		Assert.assertTrue(World.getChoices(test_World).contains("Go to upstairs"));
+		assertEquals(7, World.getChoices(test_World).size());
+	}
+	@Test
+	public void getLookChoices_LocationYard() {
+		World test_World = new World();
+		String expectedResult = "Look at yard";
+		String actualResult = World.getLookChoices(test_World);
+		assertEquals(expectedResult, actualResult);
+	}
+	@Test
+	public void getGoChoices_AllLocations() {
+		World test_World = new World();
+		Assert.assertTrue(World.getGoChoices(test_World).contains("Go to yard"));
+		Assert.assertTrue(World.getGoChoices(test_World).contains("Go to woods"));
+		Assert.assertTrue(World.getGoChoices(test_World).contains("Go to entrance"));
+		Assert.assertTrue(World.getGoChoices(test_World).contains("Go to dining room"));
+		Assert.assertTrue(World.getGoChoices(test_World).contains("Go to upstairs"));
+		assertEquals(5, World.getGoChoices(test_World).size());
+	}
 }
