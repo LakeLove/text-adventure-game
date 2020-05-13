60ef4c6fc7f1751072d5b4cea6dc0bcf19908c52
LakeLove <lakegreene@yahoo.com>
2020-05-13 00:04:47 -0400

    Created String[] neighbors field in Location class and updated equals() and hashCode().

--- src/Location.java
+++ src/Location.java
@@ -1,17 +1,24 @@
+import java.util.Arrays;
 import java.util.Objects;
 
 public class Location {
-    String about;
-    public Location(String description) {about = description;}
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (!(o instanceof Location)) return false;
-        Location location = (Location) o;
-        return Objects.equals(about, location.about);
-    }
-    @Override
-    public int hashCode() {
-        return Objects.hash(about);
-    }
+	String about;
+	String[] neighbors;
+	public Location(String description, String[] n) {
+		about = description;
+		neighbors = n;
+	}
+	@Override
+	public boolean equals(Object o) {
+		if (this == o) return true;
+		if (!(o instanceof Location)) return false;
+		Location location = (Location) o;
+		return about.equals(location.about) && Arrays.equals(neighbors, location.neighbors);
+	}
+	@Override
+	public int hashCode() {
+		int result = Objects.hash(about);
+		result = 31 * result + Arrays.hashCode(neighbors);
+		return result;
+	}
 }
